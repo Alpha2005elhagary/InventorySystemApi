bac0de02a95d5d2d39d5949b0912f8e28398eacd
Salma <salmaahmed20275@gmail.com>
2026-09-21 18:47:21 +0300

    Delete ITokenService.cs

--- ITokenService.cs
+++
@@ -1,8 +0,0 @@
-using InventorySystemApi.Models;
-
-namespace InventorySystemApi.Services;
-
-public interface ITokenService
-{
-    string GenerateToken(ApplicationUser user, IList<string> roles);
-}
